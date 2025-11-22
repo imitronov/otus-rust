b8ac4e864efbe8ea8807dccef5ff8bcb203763f5
Ivan Mitronov <imitronov@gmail.com>
2025-11-22 17:52:12 +0300

    homework 13

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-struct RingBuffer {
+use std::sync::{Arc, Mutex};
+
+struct RingBufferData {
     read_idx: usize,
     write_idx: usize,
-    data: Vec<u8>,
+    buffer: Vec<u8>,
+}
+
+struct RingBuffer {
+    data: Arc<Mutex<RingBufferData>>,
 }
 
 impl RingBuffer {
     fn create(size: usize) -> RingBuffer {
         RingBuffer {
-            read_idx: 0,
-            write_idx: 0,
-            data: vec![0; size],
+            data: Arc::new(Mutex::new(RingBufferData {
+                read_idx: 0,
+                write_idx: 0,
+                buffer: vec![0; size],
+            })),
         }
     }
 
-    fn write(rb: &mut RingBuffer, data: &str) -> Result<usize, String> {
+    fn write(&self, data: &str) -> Result<usize, String> {
         let bytes = data.as_bytes();
         let mut written = 0;
 
+        let rb = Arc::clone(&self.data);
+        let mut rb = rb.lock().unwrap();
+
         for &byte in bytes {
-            if rb.data[rb.write_idx] != 0 {
+            let write_id = rb.write_idx;
+
+            if rb.buffer[rb.write_idx] != 0 {
                 if written == 0 {
                     return Err("NoSpaceLeft".to_owned());
                 } else {
@@ -26,10 +39,10 @@ impl RingBuffer {
                 }
             }
 
-            rb.data[rb.write_idx] = byte;
+            rb.buffer[write_id] = byte;
             rb.write_idx += 1;
 
-            if rb.write_idx >= rb.data.len() {
+            if rb.write_idx >= rb.buffer.len() {
                 rb.write_idx = 0;
             }
 
@@ -39,11 +52,16 @@ impl RingBuffer {
         Ok(written)
     }
 
-    fn read(rb: &mut RingBuffer, count: usize) -> Option<String> {
+    fn read(&self, count: usize) -> Option<String> {
         let mut result = Vec::with_capacity(count);
 
+        let rb = Arc::clone(&self.data);
+        let mut rb = rb.lock().unwrap();
+
         for _ in 0..count {
-            if rb.data[rb.read_idx] == 0 {
+            let read_idx = rb.read_idx;
+
+            if rb.buffer[rb.read_idx] == 0 {
                 if result.is_empty() {
                     return None;
                 } else {
@@ -51,11 +69,11 @@ impl RingBuffer {
                 }
             }
 
-            result.push(rb.data[rb.read_idx]);
-            rb.data[rb.read_idx] = 0;
+            result.push(rb.buffer[rb.read_idx]);
+            rb.buffer[read_idx] = 0;
             rb.read_idx += 1;
 
-            if rb.read_idx >= rb.data.len() {
+            if rb.read_idx >= rb.buffer.len() {
                 rb.read_idx = 0;
             }
         }
@@ -65,10 +83,10 @@ impl RingBuffer {
 }
 
 fn main() {
-    let mut rb = RingBuffer::create(4);
+    let rb = RingBuffer::create(4);
 
-    let written = RingBuffer::write(&mut rb, "abcd");
-    let result = RingBuffer::read(&mut rb, 4);
+    let written = rb.write("abcd");
+    let result = rb.read(4);
 
     println!("{:?}", written);
     println!("{:?}", result);
@@ -80,11 +98,55 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let mut rb = RingBuffer::create(3);
-        assert_eq!(RingBuffer::write(&mut rb, "ab"), Ok(2));
-        assert_eq!(RingBuffer::write(&mut rb, "cd"), Ok(1));
-        assert_eq!(RingBuffer::read(&mut rb, 1), Some("a".to_string()));
-        assert_eq!(RingBuffer::write(&mut rb, "e"), Ok(1));
-        assert_eq!(RingBuffer::read(&mut rb, 2), Some("bc".to_string()));
+        let rb = RingBuffer::create(3);
+        assert_eq!(rb.write("ab"), Ok(2));
+        assert_eq!(rb.write("cd"), Ok(1));
+        assert_eq!(rb.read(1), Some("a".to_string()));
+        assert_eq!(rb.write("e"), Ok(1));
+        assert_eq!(rb.read(2), Some("bc".to_string()));
+    }
+
+    #[test]
+    fn multithread_stress_test() {
+        use std::sync::Arc;
+        use std::thread;
+
+        let word = "foo";
+        let word_len = word.len();
+        let clones = 10;
+        let cycles = 10_000;
+        let rb: Arc<RingBuffer> = Arc::new(RingBuffer::create(word_len * clones * cycles));
+
+        let mut handles = vec![];
+
+        for _ in 0..clones {
+            let rb = rb.clone();
+            handles.push(thread::spawn(move || {
+                for _ in 0..cycles {
+                    let _ = rb.write(word);
+                }
+            }));
+        }
+
+        for _ in 0..clones {
+            let rb = rb.clone();
+            handles.push(thread::spawn(move || {
+                for _ in 0..cycles {
+                    let out = rb.read(3);
+
+                    if let Some(s) = out {
+                        assert_eq!(s, word);
+                    }
+                }
+            }));
+        }
+
+        for h in handles {
+            h.join().unwrap();
+        }
+
+        let out = rb.read(word_len);
+
+        assert_eq!(out, None);
     }
 }
